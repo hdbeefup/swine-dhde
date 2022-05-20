fe68843a3275fe827856e9bebf288656fe31aa67
dw5 <29355584+dw5@users.noreply.github.com>
2022-05-20 22:08:00 +0300

    Level 1

--- source/SWINE.definitive/dllmain.cpp
+++ source/SWINE.definitive/dllmain.cpp
@@ -11,15 +11,13 @@ bool bEnableExperimentResPW, bModdedTanklist, bEnableCheats, bMapRotate, bUseWAS
 
 void Init()
 {
-	auto BaseAddress = GetModuleHandle(NULL);
 	auto BaseAddressptr = (uintptr_t)GetModuleHandle(NULL);
 	OutputDebugStringA("Hello HDBEEFUP!");
-	OutputDebugStringA((LPCSTR)BaseAddress);
-	OutputDebugStringA((LPCSTR)BaseAddress+0x100516+0x6CF46);
+
 	CIniReader iniReader("swinechdb.ini"); /// HDB Settings
 
 	bModdedTanklist = iniReader.ReadInteger("Mods", "TanklistMod", 0);
-	bEnableExperimentResPW = iniReader.ReadInteger("Patchworks", "EnableExperimentResPW", 1);
+	bEnableExperimentResPW = iniReader.ReadInteger("Patchworks", "EnableExperimentResPW", 0);
 	bEnableCheats = iniReader.ReadInteger("Patchworks", "EnableCheats", 0);
 	bMapRotate = iniReader.ReadInteger("Patchworks", "TESTNoMapRotate", 0);
 	bUseWASD = iniReader.ReadInteger("Patchworks", "TESTRipArrowsUseWASD", 0);
@@ -27,11 +25,13 @@ void Init()
 	// Mods
 	int ColorMM = iniReader.ReadInteger("Mods", "ColorMainMenu", 0xF0F0F0); // COLOR
 	int ColorHOV = iniReader.ReadInteger("Mods", "ColorMainMenuHOV", 0xFFFFFF); // COLOR HOVER
-	//injector::WriteMemory<int>(0x41809D, ColorMM, true); // COLOR
-	//injector::WriteMemory<int>(0x418098, ColorHOV, true); // COLOR HOVER
+	injector::WriteMemory<int>(BaseAddressptr+0xEA74F, ColorMM, true); // COLOR
+	injector::WriteMemory<int>(BaseAddressptr+0xEA754, ColorHOV, true); // COLOR HOVER
 
 	std::string MPMasterlist = iniReader.ReadString("Mods", "MultiplayerMaster", "nat.kite-games.com"); // Multiplayer hub
-    //injector::WriteMemory<DWORD>(0x5016BE, (DWORD)((char*)strdup(MPMasterlist.c_str())), true); // master.gamespy.com
+    injector::WriteMemory<DWORD>(BaseAddressptr+0x73F52, (DWORD)((char*)strdup(MPMasterlist.c_str())), true); // master.gamespy.com
+    injector::WriteMemory<DWORD>(BaseAddressptr+0x73F63, (DWORD)((char*)strdup(MPMasterlist.c_str())), true); // master.gamespy.com
+    injector::WriteMemory<DWORD>(BaseAddressptr+0x73F92, (DWORD)((char*)strdup(MPMasterlist.c_str())), true); // master.gamespy.com
 
 	// Camera 
 	CamMaxZoomIn = iniReader.ReadFloat("Camera", "MaxZoomIn", 10.0f);
@@ -41,16 +41,13 @@ void Init()
 	CamMapLimit = iniReader.ReadFloat("Camera", "MapCameraLimit", 48.0f);
 
 	//injector::WriteMemory<DWORD>(0x43A85E, (DWORD)Takeover, true);
-	std::string TakeoverMD = "%s S.W.I.N.E. v%d.%d Super Remaster | dw5 | Compiled at " __DATE__ ", " __TIME__ " |";
+	std::string TakeoverMD = "%s S.W.I.N.E. v%d.%d.%d Super Remaster | dw5 | Compiled at " __DATE__ ", " __TIME__ " |"; // S.W.I.N.E. %s v%d.%d.%d
 	//injector::WriteMemory<DWORD>(BaseAddress-0x28F77, (DWORD)((char*)strdup(TakeoverMD.c_str())), true);
-	injector::WriteMemory<DWORD>(BaseAddressptr + 0x100516 + 0x6CF46, (DWORD)((char*)strdup(TakeoverMD.c_str())), true);
-	//injector::WriteMemory<DWORD>(BaseAddressptr + 0X76C94E96, (DWORD)((char*)strdup(TakeoverMD.c_str())), true);
-	//injector::WriteMemory<DWORD>(0X00b0819f, (DWORD)((char*)strdup(TakeoverMD.c_str())), true);
-	//injector::WriteMemory<DWORD>(BaseAddress+0xD759E, (DWORD)((char*)strdup(TakeoverMD.c_str())), true);
+	injector::WriteMemory<DWORD>(BaseAddressptr + 0x6DB46, (DWORD)((char*)strdup(TakeoverMD.c_str())), true); // Relative Virtual Address... fancyyy
 	//injector::WriteMemory<unsigned char>(0x43a82d, 11, true); // SWINE Edition (byte)
 
  /* ====== Mods ====== */
-  //injector::WriteMemory<unsigned char>(0x42cc68, 0xEB, true); // Disable tamper check (JMP)
+  injector::WriteMemory<unsigned char>(BaseAddressptr+0xDF7AB, 0x85, true); // Enable Cheats 0x9bf7ab || DF7AB direct, less direct 0xDF7DB
 
   //std::string fdb_unitsini = "gamedata\\" + basepath + "\\units.ini";
   //injector::WriteMemory<DWORD>(0x42cc50, (DWORD)((char*)strdup(fdb_unitsini.c_str())), true);
@@ -58,20 +55,24 @@ void Init()
   //injector::WriteMemory<DWORD>(0x42cc50, (DWORD)((char*)"gamedata/units.ini"), true);
   //injector::WriteMemory<DWORD>(0x496add, (DWORD)((char*)"gamedata/units.ini"), true);
 
-	 /*
-  injector::WriteMemory<float>(0x4CB3A9, CamMaxZoomIn, true); // Max zoom in DEF: 10
-  injector::WriteMemory<float>(0x4CB3B3, CamMaxZoomOut, true); // Max zoom out DEF: 20
-  injector::WriteMemory<float>(0x4CB395, CamMaxAngleUp, true); // Max angle up DEF: -1.13
-  injector::WriteMemory<float>(0x4CB39F, CamMaxAngleDown, true); // Max angle down DEF: -0.7
-  injector::WriteMemory<float>(0x4CB3D5, CamMapLimit, true); // Map Cameralimit DEF: 48
-
+	 
+  injector::WriteMemory<float>(BaseAddressptr+0xCDD71, CamMaxZoomIn, true); // Max zoom in DEF: 11
+  injector::WriteMemory<float>(BaseAddressptr+0xCD874, CamMaxZoomIn, true); // Max zoom in DEF: 11
+  injector::WriteMemory<float>(BaseAddressptr+0xCDD78, CamMaxZoomOut, true); // Max zoom out DEF: 22
+  injector::WriteMemory<float>(BaseAddressptr+0xCD87B, CamMaxZoomOut, true); // Max zoom out DEF: 22
+  injector::WriteMemory<float>(BaseAddressptr+0xCDD63, CamMaxAngleUp, true); // Max angle up DEF: -1.13
+  injector::WriteMemory<float>(BaseAddressptr+0xCD866, CamMaxAngleUp, true); // Max angle up DEF: -1.13
+  injector::WriteMemory<float>(BaseAddressptr+0xCDD6A, CamMaxAngleDown, true); // Max angle down DEF: -0.7
+  injector::WriteMemory<float>(BaseAddressptr+0xCD86D, CamMaxAngleDown, true); // Max angle down DEF: -0.7
+  //injector::WriteMemory<float>(0x4CB3D5, CamMapLimit, true); // Map Cameralimit DEF: 48 || hd remaster does not have it
+   
   if (bModdedTanklist)
   {
 
 	  int i, tanks = 0, characters = 0;
 	  std::string strDL;
 	  /*			 <~!DEVS!~>				 */
-	  /*/ std::ifstream file("tanks.txt");
+	  std::ifstream file("tanks.txt");
 	  //if (bDevMode) { std::ifstream file("gamedata\\" + basepath + "\\tanks.txt"); } else { std::ifstream file("gamedata\\" + basepath + "\\tanks.txt"); }
 
 	  std::string str2;
@@ -86,7 +87,7 @@ void Init()
 		  strDL.append((64 - characters), '\0');                    // ".........."
 
 		  tanks++; /* CONFIRMED: TANKS COUNTER AND WRITING WORKS PROPERLY AS IT SHOULD, thus problem is with final write, not reading*/
-		  /*/ characters = 0;
+		  characters = 0;
 	  }
 
 	  //injector::WriteMemory<DWORD>(0x409ef4, (DWORD)((char*)strdup(fdb_menuxxx.c_str())), true);
@@ -95,11 +96,11 @@ void Init()
 	  // also theres lil glitch making SP: 13, starting mission lets you use unspemt (eg. had 1000, spent 10, remains 3, inmission you can spend rest 900)
 	  // increasing via cheats, causes anif error for some tanks. clicking too fast into 3rd slider, same. plus glitched icons, sometimes uses even background graphics.
 
-	  injector::WriteMemory<unsigned char>(0x41995c, tanks, true); // ICON? HOW MANY TANKS DO WE HAVE FOR USE IN CAMPAIGN? (/p-01)
+	 /* injector::WriteMemory<unsigned char>(0x41995c, tanks, true); // ICON? HOW MANY TANKS DO WE HAVE FOR USE IN CAMPAIGN? (/p-01)
 	  injector::WriteMemory<unsigned char>(0x419c62, tanks, true); // ICON (e.g. briefing) max 127
 	  injector::WriteMemory<unsigned char>(0x42878e, tanks, true); // MARKET STORE CLASS NAMES. Max 127
 	  injector::WriteMemory<unsigned char>(0x42480f, tanks, true); // market BUY SIDE AVAILABLE FOR PURCHASE (too much might give a crash). 
-	  //^causes money glitch. selling something will give buy side anif error IF too many characters in unit mask
+	  *///^causes money glitch. selling something will give buy side anif error IF too many characters in unit mask
 	  // DO NOT DUPLICATE (e.g. Service, fuel, ammo)
 	  //injector::WriteMemory<unsigned char>(0x42420B, tanks, true); // <!-- Problem maker, and a very useless one market icon loader !instant crash, lower value like 14 does nothing. high invalid child parameter.
 	  // seems to allow to have third slider? nah
@@ -116,13 +117,33 @@ void Init()
 	  char* aNyulDzsip = new char[nyulstringlen + 1];
 	  std::copy(strDL.begin(), strDL.end(), aNyulDzsip);
 	  aNyulDzsip[nyulstringlen] = '\0';
-
+	  
 	  // calls to spot where campaign units are listed
-	  injector::WriteMemory<DWORD>(0x4199ef, (DWORD)aNyulDzsip, true);
+	  //injector::WriteMemory<DWORD>(BaseAddressptr + 0xEFB6D, (DWORD)aNyulDzsip, true); // MARKET 3 CRASH
+	  //injector::WriteMemory<DWORD>(BaseAddressptr + 0xF058D, (DWORD)aNyulDzsip, true); // CRASH ON INGAME BUY
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xF4B49, (DWORD)aNyulDzsip, true);
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xEA2EA, (DWORD)aNyulDzsip, true); // EA2EA 
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xF488C, (DWORD)aNyulDzsip, true);
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xF3E20, (DWORD)aNyulDzsip, true);
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xF3254, (DWORD)aNyulDzsip, true);
+	  //injector::WriteMemory<DWORD>(BaseAddressptr + 0x9ADFD, (DWORD)aNyulDzsip, true); // hI i WaNnA CrAsH
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xEF906, (DWORD)aNyulDzsip, true); // MARKET 2 crash
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xEF8B0, (DWORD)aNyulDzsip, true); // MARKET 1 crash aka both
+
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xEA1BC, (DWORD)aNyulDzsip, true);
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xEA14E, (DWORD)aNyulDzsip, true);
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xEA0DA, (DWORD)aNyulDzsip, true);
+	  //injector::WriteMemory<DWORD>(BaseAddressptr + 0xF071F, (DWORD)aNyulDzsip, true); // CRASH ON INGAME BUY
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xF0160, (DWORD)aNyulDzsip, true);
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xEF95B, (DWORD)aNyulDzsip, true);
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xEA35D, (DWORD)aNyulDzsip, true);
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xEA392, (DWORD)aNyulDzsip, true);
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xEA116, (DWORD)aNyulDzsip, true);
+	  injector::WriteMemory<DWORD>(BaseAddressptr + 0xF08D0, (DWORD)aNyulDzsip, true);
 	  //injector::WriteMemory<DWORD>(0x5431B8, (DWORD)aNyulDzsip, true); // 0x5431B8 .. 0x5431b0 not worth to use
-/*/
-/* }
-  */
+
+ }
+  
 }
 
 
